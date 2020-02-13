78523e8cdc200cdff13799e3fb4cde818f0f47f2
andthiel_l <acthiel212@gmail.com>
2020-02-13 14:03:00 -0600

    Fixed error in reference platform logging

--- platforms/reference/src/ReferenceGKNPKernels.cpp
+++ platforms/reference/src/ReferenceGKNPKernels.cpp
@@ -212,14 +212,14 @@ double ReferenceCalcGKNPForceKernel::executeGVolSA(ContextImpl& context, bool in
 #endif
 
 
-
+    volume2=0;
     for(int i = 0; i < numParticles; i++){
       force[i] += vol_force[i] * w_evol;
       printf("self_volume: %6.6f atom: %d\n", self_volume[i], i);
       volume2=+self_volume[i];
       printf("vol_energies[%d]: %6.6f\n", i, self_volume[i]*nu[i]);
     }
-    
+
     printf("inputGamma: %6.9f\nRoffset: %6.9f\nVolume1: %6.9f\nVolume2: %6.9f\n", gammas[0], roffset, volume1, volume2);
     double totalEnergy= (gammas[0] / roffset * (volume1)- gammas[0] / roffset *(volume2))*.4184;
     printf("Energy from self volumes: %6.9f\n",totalEnergy);
